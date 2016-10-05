233f769450c41653142f26540f7d663e9e4343da
Haimal Khetan <Haimal@haimals-macbook-pro.local>
2016-10-06 00:56:02 +0530

    Initial Commit

---
+++ MagicPills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicPills
+//
+//  Created by Haimal Khetan on 06/10/16.
+//  Copyright © 2016 Haimal Khetan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
